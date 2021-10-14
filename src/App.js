6fbcc439492efb43ffce7d31c3252f6a9f77e7ba
AnJiIn <aji0719@gmail.com>
2021-10-15 00:05:47 +0900

    Making The Movie App ~Rendering

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from './Movie';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Cookie",
-    picture: "🍪",
-    rating: 4.2
-  },
-  {
-    id: 2,
-    name: "Pizza",
-    picture: "🍕",
-    rating: 4.1
-  },
-  {
-    id: 3,
-    name: "Cake",
-    picture: "🍰",
-    rating: 4.7
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() => {
+    const {data: { data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false })
+  };
+  componentDidMount(){
+    this.getMovies();
   }
-];
-
-function Food ({name, picture, rating}){
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h3>{picture}</h3>
-      <h4>{rating} / 5.0</h4>
+  render(){
+    const {isLoading, movies} = this.state;
+    return <div>
+      {isLoading ? "Loading..." : movies.map(movie => {
+        console.log(movie)
+        return (
+        <Movie 
+          key={movie.id} 
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image} 
+        />);
+      })}
     </div>
-  );
+  };
 };
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      <h2>My Favorite Food</h2>
-      {foodILike.map(dish => (<Food 
-      key={dish.id} 
-      name={dish.name} 
-      picture={dish.picture}
-      rating={dish.rating} />))}
-    </div>
-  );
-}
 
 export default App;
